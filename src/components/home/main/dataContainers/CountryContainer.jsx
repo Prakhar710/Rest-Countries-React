6e8405d21f0d1e459744455add6de5fd9bc1009e
Prerit Mathur <mathur.prerit@gmail.com>
2020-06-11 12:31:24 +0530

    fixed homepage css

--- src/components/home/main/dataContainers/CountryContainer.jsx
+++ src/components/home/main/dataContainers/CountryContainer.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const CountryContainer=(props)=> {
-    console.log(props)
-    return (
+const CountryContainer = (props) => {
+  return (
+    <div className="w-25 p-2">
+      <div className="image-container">
+        <img src={props.flag} alt={props.code} className="img-fluid rounded flag-image"/>
+      </div>
+      <div>
+        <h5>{props.name}</h5>
         <div>
-            {/* <div><img src={props.flag} alt={props.code}/></div> */}
-            <div>
-    <h5>{props.name}</h5>
-    <div>
-    <div>{props.population}</div>
-    <div>{props.region}</div>
-    <div>{props.capital}</div>
-    </div>
-            </div>
-            
+          <div>{props.population}</div>
+          <div>{props.region}</div>
+          <div>{props.capital}</div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default CountryContainer
+export default CountryContainer;
--- src/components/home/main/dataContainers/DataContainer.jsx
+++ src/components/home/main/dataContainers/DataContainer.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import CountryContainer from "./CountryContainer.jsx"
+import CountryContainer from "./CountryContainer.jsx";
 
-const DataContainer=(props)=> {
-    return (
-        <div>
-            {props.countries.map((country,index)=>{
-            return <CountryContainer name={country.name} code={country.alpha3Code} key={country.alpha3Code} population={country.population} region={country.region} capital={country.capital} flag={country.flag}/>
-            })}
-        </div>
-    )
-}
+const DataContainer = (props) => {
+  return (
+    <div className="container d-flex flex-wrap align-self-stretch">
+      {props.countries.map((country, index) => {
+        return (
+            <CountryContainer
+              name={country.name}
+              code={country.alpha3Code}
+              key={country.alpha3Code}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              flag={country.flag}
+            />
+        );
+      })}
+    </div>
+  );
+};
 
-export default DataContainer
+export default DataContainer;
